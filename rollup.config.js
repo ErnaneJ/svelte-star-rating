210b49cabb2bac25c35244d3e66167b3db43ac02
Ernane Ferreira <ernane.junior25@gmail.com>
2024-03-30 13:31:13 -0300

    fix: dependencies and add ignore file

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
+import css from "rollup-plugin-css-only";
 
 const pkg = require("./package.json");
 
 export default {
-    input: "src/AtButton.svelte",
-    output: [
-        { file: pkg.module, format: "es" },
-        { file: pkg.main, format: "umd", name: "Name" },
-    ],
-    plugins: [svelte(), resolve()],
+  input: "./src/Stars.svelte",
+  output: [
+    { file: pkg.module, format: "es" },
+    { file: pkg.main, format: "umd", name: "Name" },
+  ],
+  plugins: [svelte(), resolve(), css()],
 };
